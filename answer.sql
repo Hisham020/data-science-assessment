23ec13e86794a0655c5e021071b2e9aebcc724d7
Hisham020 <heshamalyani.2000@gmail.com>
2025-11-08 14:42:02 +0300

    Update answer.sql

--- answer.sql
+++ answer.sql
@@ -1,1 +1,14 @@
--- ADD YOUR SQL QUERY HERE
+...
+SELECT
+    t1.investor_id,
+    t2.sector_name,
+    ROUND(
+        (t1.no_of_shares * 100.0) / 
+        SUM(t1.no_of_shares) OVER (PARTITION BY t1.investor_id), -- Key line
+        2
+    ) AS share_percentage
+FROM
+    investor_transactions t1
+JOIN
+    sectors t2 ON t1.sector_id = t2.sector_id
+...
